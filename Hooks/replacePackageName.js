8d1b5d0dfa2541354c99c1383bd1bacd7d366e0f
Luis Bouça <luis.bouca@outsystems.com>
2021-01-27 15:27:48 +0000

    Fix: add videoDisplay to hook scope

--- Hooks/replacePackageName.js
+++ Hooks/replacePackageName.js
@@ -6,6 +6,20 @@ var appId = "";
 function replacer(match, p1,p2, p3, offset, string){
     return [p1,appId,".R",p3].join("");
 }
+
+function replacePackageInPath(path){
+    if (fs.existsSync(path)) {
+        var file = fs.readFileSync(path, "utf8");
+        var regex = /([\s|\S]*)(com\.outsystems\.sample.R)([\s|\S]*)/gm;
+        file = file.replace(regex,replacer);
+
+        
+        fs.writeFileSync(path, file);
+        console.log("Finished changing the packageNames!");
+    }else{
+        console.log("Error could not find packageNames!");
+    }
+}
 module.exports = function (context) {
     
     console.log("Start changing Code Files!");
@@ -25,18 +39,10 @@ module.exports = function (context) {
     id = id.replace(regexId,"/")
 
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
-    var activityFilePath = path.join(projectRoot,"platforms","android","app","src","main","java","com","outsystems","sumnisdk","present","TextDisplay.java");
-    if (fs.existsSync(activityFilePath)) {
-        var activity = fs.readFileSync(activityFilePath, "utf8");
-        var regex = /([\s|\S]*)(com\.outsystems\.sample.R)([\s|\S]*)/gm;
-        activity = activity.replace(regex,replacer);
-
-        
-        fs.writeFileSync(activityFilePath, activity);
-        console.log("Finished changing the packageNames!");
-    }else{
-        console.log("Error could not find packageNames!");
-    }
+    var textFilePath = path.join(projectRoot,"platforms","android","app","src","main","java","com","outsystems","sumnisdk","present","TextDisplay.java");
+    var videoFilePath = path.join(projectRoot,"platforms","android","app","src","main","java","com","outsystems","sumnisdk","present","VideoDisplay.java");
+    replacePackageInPath(textFilePath);
+    replacePackageInPath(videoFilePath);
     deferral.resolve();
 
     return deferral.promise;
